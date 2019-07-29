7698a4b59f4c746b458622a75f2dca6c36943f98
unknown <Olurin anjola>
2019-07-30 00:25:34 +0100

    M

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', 'http://buildeasy-supplier.herokuapp.com')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Credentials', false)
+            ->header('Access-Control-Allow-Credentials:true')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-CSRF-Token, Authorization');
     }
