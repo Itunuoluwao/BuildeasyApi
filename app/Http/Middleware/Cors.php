61ce69b7f1852787ee90d3f18f6c153b43706239
unknown <Olurin anjola>
2019-07-30 00:18:49 +0100

    M

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,10 +16,10 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', 'http://buildeasy-supplier.herokuapp.com')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, origin, accept, X-API-KEY')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-CSRF-Token, Authorization');
     }
 }
