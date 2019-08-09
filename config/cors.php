68dd09bdc3e6ab9e40e3f1964e2a38e9d9cf569a
unknown <Olurin anjola>
2019-08-09 01:20:18 +0100

    M

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
 
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['http://buildeasy-supplier.herokuapp.com'],
     'allowedHeaders' => ['*'],
     'allowedMethods' =>  ['GET', 'POST', 'PUT',  'DELETE','INCLUDE'],
     'exposedHeaders' => [],
